ba98796d93a9cdd85e6151b80bbaa9897b21cbbf
duxinwu <13935741214@163.com>
2022-04-20 18:30:11 +0800

    feat: string-literal-types

---
+++ src/Advanced/string-literal-types/01.ts
@@ -0,0 +1,18 @@
+/**
+ * 字符串字面量类型
+ * 用来约束取值只能是某几个字符串中的一个
+ * 类型别名与字符串字面量类型都是使用 type 进行定义
+ */
+
+/*
+  type FriendsName = 'dxw' | 'da' | 'hr';
+  function getName(name: FriendsName): string {
+    return name;
+  }
+  console.log(getName('dxw'));
+ */
+
+/*
+  type arr = 1 | 2 | '3';
+  const arg: arr = 2;
+ */
--- src/Advanced/type-aliases/01.ts
+++ src/Advanced/type-aliases/01.ts
@@ -5,13 +5,15 @@
  * 常用于联合类型
  */
 
-type Name = string;
-type NameResolver = () => string;
-type NameOrResolver = Name | NameResolver;
-function getName(n: NameOrResolver): Name {
-  if (typeof n === 'string') {
-    return n;
-  } else {
-    return n();
+/*
+  type Name = string;
+  type NameResolver = () => string;
+  type NameOrResolver = Name | NameResolver;
+  function getName(n: NameOrResolver): Name {
+    if (typeof n === 'string') {
+      return n;
+    } else {
+      return n();
+    }
   }
-}
+ */
